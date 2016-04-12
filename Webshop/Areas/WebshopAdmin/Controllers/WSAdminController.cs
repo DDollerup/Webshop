481db4b55f9e1884f6c2368c26436abf28439a96
PDDaniel <daniel@proper-dog.com>
2016-04-12 09:16:56 +0200

    Added Area and Item site to show all items

---
+++ Webshop/Areas/WebshopAdmin/Controllers/WSAdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webshop.Models.BaseModels;
+using Webshop.Factories;
+
+namespace Webshop.Areas.WebshopAdmin.Controllers
+{
+    public class WSAdminController : Controller
+    {
+        ItemFactory itemFac = new ItemFactory();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Item()
+        {
+            List<Item> allItems = itemFac.GetAll();
+            return View(allItems);
+        }
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddItemSubmit(Item item)
+        {
+            // Add Database Function
+            return RedirectToAction("Item");
+        }
+
+        public ActionResult EditItem(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditItemSubmit(Item item)
+        {
+            return RedirectToAction("Item");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteItemSubmit(int id)
+        {
+            return RedirectToAction("Item");
+        }
+    }
+}
---
+++ Webshop/Areas/WebshopAdmin/WebshopAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Webshop.Areas.WebshopAdmin
+{
+    public class WebshopAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WebshopAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WebshopAdmin_default",
+                "WebshopAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
